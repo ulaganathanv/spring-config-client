aedd0809e7cf22ed9faca4c7ac5a7a783bc465bc
Ulaganathan Vallinayagam <ulag.india@gmail.com>
2019-08-29 14:11:55 +0530

    Base Version

---
+++ src/main/java/com/example/client/springconfigclient/SpringconfigclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.client.springconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringconfigclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringconfigclientApplication.class, args);
+	}
+
+}
